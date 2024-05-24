ebacd8667aeb5165049693cf642fdc76ad33da3f
schulle334 <62450965+schulle334@users.noreply.github.com>
2024-05-24 14:14:05 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,14 @@
-# Arda Mavi
 import numpy as np
-from scipy.misc import imresize
+from skimage.transform import resize
 
 def predict(model, X):
-    X = imresize(X, (150, 150, 3)).astype('float32')/255.
-    Y = model.predict(X.reshape(1,150,150,3))
+    # Ändern der Bildgröße mit skimage.transform.resize
+    X_resized = resize(X, (150, 150, 3), anti_aliasing=True).astype('float32')
+
+    # Normalisieren der Pixelwerte
+    X_normalized = X_resized / 255.0
+
+    # Vorhersage mit dem Modell
+    Y = model.predict(np.expand_dims(X_normalized, axis=0))
+
     return Y
